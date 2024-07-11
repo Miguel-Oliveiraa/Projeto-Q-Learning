956221c86568b24cef313ec64c545c066eb896f3
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-07-10 22:59:20 -0300

    feat: treinamento while true

--- client.py
+++ client.py
@@ -33,79 +33,48 @@ def main():
         except:
             Q = np.zeros([24 * 4, 3])
         
-        rewards_per_episode = []
-        
         #initialize the exploration probability to 1
-        exploration_proba = 1
-
-        #exploartion decreasing decay for exponential decreasing
-        exploration_decreasing_decay = 0.001
-
-        # minimum of exploration proba
-        min_exploration_proba = 0.01
+        exploration_proba = 0.2
 
         #discounted factor
-        gamma = 1
+        gamma = 0.6
 
         #learning rate
-        lr = 1
+        lr = 0.9
         
         moves = ["left", "right", "jump"]
         acao = 2
 
-        #number of episode we will run
-        n_episodes = 10
+        print_values(lr, gamma, exploration_proba)
+        state, reward = cn.get_state_reward(c, "")
+        plataforma = int(state[2:6], 2)
+        direction = int(state[-2:], 2)
+        total_episode_reward = 0
+        # Game
+        print("\nInitial State: "+state+"\nPlataforma: " + str(plataforma)+"\nDireção: " + str(direction)+"\nreward: "+str(reward)+"\n")
 
-        #maximum of iteration per episode
-        iter_per_episode = 100
-        # Loop until lr reaches 0
-        while lr >= 0:
-            while gamma >= 0:
-                while exploration_proba >= 0:
-                    print_values(lr, gamma, exploration_proba)
-                    for e in range (n_episodes):
-                        Q = np.zeros([24 * 4, 3])
-                        state, reward = cn.get_state_reward(c, "")
-                        plataforma = int(state[2:6], 2)
-                        direction = int(state[-2:], 2)
-                        total_episode_reward = 0
-                        # Game
-                        print("\nInitial State: "+state+"\nPlataforma: " + str(plataforma)+"\nDireção: " + str(direction)+"\nreward: "+str(reward)+"\n")
+        while(True):   
+            # Converte o state em um número inteiro
+            state_code = int(state, 2) 
 
-                        for (_) in range(iter_per_episode):       
+            acao = move(Q, exploration_proba, state_code)
+            
+            # Executa ação e recebe state e reward
+            next_state, reward = cn.get_state_reward(c, moves[acao])
+            next_state_code = int(next_state, 2)            
+            plataforma = int(next_state[2:6], 2)
+            direction = int(next_state[-2:], 2)
+            print("Ação efetuada: " + moves[acao] + "\nnew state: "+next_state+"\nPlataforma: " + str(plataforma)+"\nDireção: " + str(direction)+"\nreward: "+str(reward)+"\n")
 
-                            # Converte o state em um número inteiro
-                            state_code = int(state, 2) 
+            # Update Q-table
+            Q[state_code, acao] = (1 - lr) * Q[state_code, acao] + lr * (reward + gamma * np.max(Q[next_state_code, :]))              
+            np.savetxt('resultado.txt', Q)
 
-                            acao = move(Q, exploration_proba, state_code)
-                            
-                            # Executa ação e recebe state e reward
-                            next_state, reward = cn.get_state_reward(c, moves[acao])
-                            next_state_code = int(next_state, 2)            
-                            plataforma = int(next_state[2:6], 2)
-                            direction = int(next_state[-2:], 2)
-                            print("Ação efetuada: " + moves[acao] + "\nnew state: "+next_state+"\nPlataforma: " + str(plataforma)+"\nDireção: " + str(direction)+"\nreward: "+str(reward)+"\n")
-
-                            # Update Q-table
-                            Q[state_code, acao] = (1 - lr) * Q[state_code, acao] + lr * (reward + gamma * np.max(Q[next_state_code, :]))              
-                            np.savetxt('resultado.txt', Q)
-
-                            total_episode_reward = total_episode_reward + reward
-                            # Update state
-                            state = next_state
-                        
-                        exploration_proba = max(min_exploration_proba, np.exp(-exploration_decreasing_decay*e))
-                        rewards_per_episode.append(total_episode_reward)
-                    print_rewards(rewards_per_episode)
-                    rewards_per_episode = []
-                    exploration_proba -= 0.1
-                gamma -= 0.1
-                exploration_proba = 1.0
-            lr -= 0.1
-            gamma = 1.0
-            exploration_proba = 1.0
-
+            total_episode_reward = total_episode_reward + reward
+            # Update state
+            state = next_state
         
+        # exploration_proba = max(min_exploration_proba, np.exp(-exploration_decreasing_decay*e))
     else:
         print('Connection failed!')
     
